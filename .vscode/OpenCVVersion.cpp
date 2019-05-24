6d9f862cf121f921524e5775c9f0a085bd7f1c64
Levid Rodriguez <levid.rodriguez@gmail.com>
2019-05-24 12:44:32 -0500

    OpenCV version

---
+++ .vscode/OpenCVVersion.cpp
@@ -0,0 +1,19 @@
+%%cu
+// C++
+#include <iostream>
+#include <vector>
+
+//OpenCV
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/stitching.hpp>
+#include <opencv2/core/utility.hpp>  
+
+int main( int argc, char** argv )
+{  
+    std::cout<<"Hola Mundo"<<std::endl; 
+    std::cout << "OpenCV version : " << CV_VERSION << std::endl;
+    std::cout << "Major version : " << CV_MAJOR_VERSION << std::endl;
+    std::cout << "Minor version : " << CV_MINOR_VERSION << std::endl;
+    std::cout << "Subminor version : " << CV_SUBMINOR_VERSION << std::endl;
+}
