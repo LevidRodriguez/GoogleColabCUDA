23bb2cb0aa4ecd2c921dc439a059b0877590dd3d
Armando Levid Rodriguez Santiago <levid.rodriguez@gmail.com>
2019-05-27 16:10:33 -0500

    Add files via upload

--- deviceQuery.cpp
+++ deviceQuery.cpp
@@ -35,9 +35,10 @@
 int main(int argc, char **argv)
 {
     std::cout<<"Starting..."<<argv[0]<<std::endl<<std::endl;
-    std::cout<<" CUDA Device Query (Runtime API) version (CUDART static linking)"<<std::endl<<std::endl;
+    std::cout<<"CUDA Device Query (Runtime API) version (CUDART static linking)"<<std::endl<<std::endl;
     int deviceCount = 0;
     cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+    
     if (error_id != cudaSuccess){
         std::cout<<"cudaGetDeviceCount returned "<< (int)error_id<< cudaGetErrorString(error_id)<<std::endl;
         std::cout<<"Result = FAIL"<<std::endl;
@@ -47,23 +48,22 @@ int main(int argc, char **argv)
     if (deviceCount == 0)
         std::cout<<"There are no available device(s) that support CUDA"<<std::endl;
     else
-        std::cout<<"Detected "<< deviceCount<<"CUDA Capable device(s)"<<std::endl<<std::endl;
+        std::cout<<"Detected "<< deviceCount<<" CUDA Capable device(s)"<<std::endl<<std::endl;
     int dev, driverVersion = 0, runtimeVersion = 0;
     for (dev = 0; dev < deviceCount; ++dev){
         cudaSetDevice(dev);
         cudaDeviceProp deviceProp;
         cudaGetDeviceProperties(&deviceProp, dev);
-        std::cout<<"Device: "<< dev<<deviceProp.name<<std::endl;
+        std::cout<<"Device: "<<dev<<" "<<deviceProp.name<<std::endl;
         // Console log
         cudaDriverGetVersion(&driverVersion);
         cudaRuntimeGetVersion(&runtimeVersion);
         std::cout<<"CUDA Driver Version / Runtime Version          "<< driverVersion/1000
                 << (driverVersion%100)/10<<runtimeVersion/1000<<(runtimeVersion%100)/10<<std::endl;
-        std::cout<<"CUDA Capability Major/Minor version number:          "<< driverVersion/1000
+        std::cout<<"CUDA Capability Major / Minor version number:          "<< driverVersion/1000
                 << deviceProp.major<<deviceProp.minor<<std::endl;
     }
-    // Check OpenCV Version
-    std::cout<<"Hola Mundo"<<std::endl; 
+    // Check OpenCV Version 
     std::cout << "OpenCV version : " << CV_VERSION << std::endl;
     std::cout << "Major version : " << CV_MAJOR_VERSION << std::endl;
     std::cout << "Minor version : " << CV_MINOR_VERSION << std::endl;
@@ -72,14 +72,14 @@ int main(int argc, char **argv)
     cv::Mat image;
     cv::Mat E = cv::Mat::eye(4,4,CV_64F);
     std:: cout<<"E = "<< std::endl<<" "<<E<<std::endl<<std::endl;
-
     image = cv::imread("images/HappyFish.jpg");
     if (!image.data){
         std::cout<<"Could not open or find the image"<<std::endl;
+        return -1;
     }
     std::cout<<"Image open!"<<std::endl;
-    
-    // cv::namedWindow("Display image", WINDOW_AUTOSIZE);
-    // cv::imshow("Display image", image);
-    // cv::waitKey();
+    cv::Mat imgGrayScale;
+    cv::cvtColor(imgGrayScale, image, cv::COLOR_BGR2GRAY);    
+    cv::imwrite("srcImageGrayScale.png",imgGrayScale);
+    return 0;
 }
