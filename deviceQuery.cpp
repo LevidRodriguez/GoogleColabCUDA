90a9bea5e2ea2434307a6b7fc07f4960761e4347
Armando Levid Rodriguez Santiago <levid.rodriguez@gmail.com>
2019-05-28 18:58:29 -0500

    Add files via upload

--- deviceQuery.cpp
+++ deviceQuery.cpp
@@ -37,18 +37,13 @@
 using namespace std;
 using namespace cv;
 
-bool try_use_gpu = false;
-bool divide_images = false;
 cv::Stitcher::Mode mode = cv::Stitcher::PANORAMA;
 std::vector<cv::Mat> imgs;
-std::string result_name = "result.jpg";
-
-void printUsage(char** argv);
-int parseCmdArgs(int argc, char** argv);
-
+std::string result_name = "resultDJI.jpg";
 
 int main(int argc, char **argv)
 {
+    // CUDA Drivers
     std::cout<<"Starting..."<<argv[0]<<std::endl<<std::endl;
     std::cout<<"CUDA Device Query (Runtime API) version (CUDART static linking)"<<std::endl<<std::endl;
     int deviceCount = 0;
@@ -97,127 +92,32 @@ int main(int argc, char **argv)
     cv::cvtColor(image, imgGrayScale,cv::COLOR_BGR2GRAY);    
     cv::imwrite("grayScale.png",imgGrayScale);
     std::cout<<"Writed Image"<<std::endl;
-
-    for (int i = 1; i < argc; ++i) 
-    { 
-            // Read the ith argument or image  
-            // and push into the image array 
-            Mat img = imread(argv[i]); 
-            if (img.empty()) 
-            { 
-                // Exit if image is not present 
-                cout << "Can't read image '" << argv[i] << "'\n"; 
-                return -1; 
-            } 
-            imgs.push_back(img); 
+    
+    // Stitching de Imagenes
+    for (int i = 1; i < argc; ++i) { 
+        cv::Mat img = cv::imread(argv[i]); 
+        if (img.empty()) { 
+            // Exit if image is not present 
+            cout << "Can't read image '" << argv[i] << "'\n"; 
+            return -1; 
+        } 
+        cv::Size size(1280,720);
+        cv::resize(img, img, size);
+        imgs.push_back(img); 
     } 
-      
     // Define object to store the stitched image 
-    Mat pano; 
+    cv::Mat pano; 
     // Create a Stitcher class object with mode panoroma 
-    Ptr<Stitcher> stitcher = Stitcher::create(mode, false); 
+    cv::Ptr<cv::Stitcher> stitcher = cv::Stitcher::create(mode, false); 
     // Command to stitch all the images present in the image array 
-    Stitcher::Status status = stitcher->stitch(imgs, pano); 
-    if (status != Stitcher::OK) 
-    { 
-        // Check if images could not be stiched 
-        // status is OK if images are stiched successfully 
+    cv::Stitcher::Status status = stitcher->stitch(imgs, pano); 
+    if (status != cv::Stitcher::OK) { 
         cout << "Can't stitch images\n"; 
         return -1; 
     } 
     // Store a new image stiched from the given  
     //set of images as "result.jpg" 
-    imwrite("result.jpg", pano); 
+    cv::imwrite("result.jpg", pano); 
+    std::cout<<"Results is written"<<std::endl;
     return 0;
 }
-
-/*
-void printUsage(char** argv){
-    cout <<
-         "Images stitcher.\n\n" << "Usage :\n" << argv[0] <<" [Flags] img1 img2 [...imgN]\n\n"
-         "Flags:\n"
-         "  --d3\n"
-         "      internally creates three chunks of each image to increase stitching success\n"
-         "  --try_use_gpu (yes|no)\n"
-         "      Try to use GPU. The default value is 'no'. All default values\n"
-         "      are for CPU mode.\n"
-         "  --mode (panorama|scans)\n"
-         "      Determines configuration of stitcher. The default is 'panorama',\n"
-         "      mode suitable for creating photo panoramas. Option 'scans' is suitable\n"
-         "      for stitching materials under affine transformation, such as scans.\n"
-         "  --output <result_img>\n"
-         "      The default is 'result.jpg'.\n\n"
-         "Example usage :\n" << argv[0] << " --d3 --try_use_gpu yes --mode scans img1.jpg img2.jpg\n";
-}
-int parseCmdArgs(int argc, char** argv){
-    if (argc == 1)
-    {
-        printUsage(argv);
-        return EXIT_FAILURE;
-    }
-    for (int i = 1; i < argc; ++i)
-    {
-        if (string(argv[i]) == "--help" || string(argv[i]) == "/?")
-        {
-            printUsage(argv);
-            return EXIT_FAILURE;
-        }
-        else if (string(argv[i]) == "--try_use_gpu")
-        {
-            if (string(argv[i + 1]) == "no")
-                try_use_gpu = false;
-            else if (string(argv[i + 1]) == "yes")
-                try_use_gpu = true;
-            else
-            {
-                cout << "Bad --try_use_gpu flag value\n";
-                return EXIT_FAILURE;
-            }
-            i++;
-        }
-        else if (string(argv[i]) == "--d3")
-        {
-            divide_images = true;
-        }
-        else if (string(argv[i]) == "--output")
-        {
-            result_name = argv[i + 1];
-            i++;
-        }
-        else if (string(argv[i]) == "--mode")
-        {
-            if (string(argv[i + 1]) == "panorama")
-                mode = Stitcher::PANORAMA;
-            else if (string(argv[i + 1]) == "scans")
-                mode = Stitcher::SCANS;
-            else
-            {
-                cout << "Bad --mode flag value\n";
-                return EXIT_FAILURE;
-            }
-            i++;
-        }
-        else
-        {
-            Mat img = imread(cv::samples::findFile(argv[i]));
-            if (img.empty())
-            {
-                cout << "Can't read image '" << argv[i] << "'\n";
-                return EXIT_FAILURE;
-            }
-            if (divide_images)
-            {
-                Rect rect(0, 0, img.cols / 2, img.rows);
-                imgs.push_back(img(rect).clone());
-                rect.x = img.cols / 3;
-                imgs.push_back(img(rect).clone());
-                rect.x = img.cols / 2;
-                imgs.push_back(img(rect).clone());
-            }
-            else
-                imgs.push_back(img);
-        }
-    }
-    return EXIT_SUCCESS;
-}
-*/
